0ccaf8f19817c14d9aefcc26d6c3d8c08ceeeb8f
Mohammad Shahnawaz <msnazia2012@gmail.com>
2025-12-20 14:14:57 +0530

    index-function-list

---
+++ src/16Dec2025/lists/function/index_function_list.py
@@ -0,0 +1,11 @@
+marks=[100,30,50,40,90,23,100]
+print('🚀 Marks')
+print(marks)
+print('🚀 Find the index of 50 in the marks list')
+print(marks.index(50))
+print('🚀 Find the index of 23 in the marks list')
+print(marks.index(23))
+print('🚀 Find the occurence of 40 in the marks list')
+print(marks.index(40))
+print('🚀 Find the occurence of 140 in the marks list')
+print(marks.index(140)) # ValueError 
