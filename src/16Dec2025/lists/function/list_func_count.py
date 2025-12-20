e2f2467666fadc9daadb09815647364bd68945fe
Mohammad Shahnawaz <msnazia2012@gmail.com>
2025-12-20 21:25:11 +0530

    list-function

---
+++ src/16Dec2025/lists/function/list_func_count.py
@@ -0,0 +1,8 @@
+marks=[100,30,50,40,90,23,100]
+print('🚀 Marks')
+print('🚀 Find the occurence of 100 in the marks list')
+print(marks.count(100))
+print('🚀 Find the occurence of 40 in the marks list')
+print(marks.count(40))
+print('🚀 Find the occurence of 70 in the marks list')
+print(marks.count(70))
