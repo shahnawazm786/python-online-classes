c54b57968895cb72814f0f92da33225c5f8739aa
Mohammad Shahnawaz <msnazia2012@gmail.com>
2026-01-01 20:52:42 +0530

    tuple examples

---
+++ src/tuples/nested_tuple.py
@@ -0,0 +1,10 @@
+fruits=('manago',('Guava','papaya','orange'),'dates','peer')
+print(fruits)
+print(fruits[1])
+print(fruits[1][1])
+fruits1=('Apple',['kolkatta','delhi','hydrabad'])
+fruits1[1][0]='Mumbai'
+print(fruits1)
+fruits1[0]='Peer'
+print(fruits1)
+
---
+++ src/tuples/tuple_index.py
@@ -0,0 +1,4 @@
+T=('mango','apple','banana')
+print(T[0])
+print(T[::-1])
+print(T[0:2])
