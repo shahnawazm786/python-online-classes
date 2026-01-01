61e1a80e9ff6904359db86a944e7d554099d6cfd
Mohammad Shahnawaz <msnazia2012@gmail.com>
2026-01-01 20:51:29 +0530

    tuple

---
+++ src/16Dec2025/lists/slicing/list_slicing_rev.py
@@ -0,0 +1,12 @@
+L=[10,5,20,-1,30,7,40,50,60]
+print(L)
+#print(L[::-1]) #::-1 -> rigt to left 
+# start -1 index stop at the last index jumping with -1
+# -1+-1 (-2) ... till end
+rev=L[::-1]
+print("REVERSE")
+print(rev)
+# palindrome
+#madam # madam
+
+
