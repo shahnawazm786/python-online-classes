447b3c45460dca15d641a119564460c6f3001526
Mohammad Shahnawaz <msnazia2012@gmail.com>
2026-01-02 21:44:37 +0530

    sets mathematical calculation

---
+++ src/01Jan2026/sets/sets_function/mathematical_set_function/set_instersect.py
@@ -0,0 +1,10 @@
+A={10,20,30}
+B={30,40,50}
+#common value show SetA and SetB 
+#intersection
+# & for intersection
+# intersection
+intersect= A & B 
+print(intersect)
+print('By method')
+print(A.intersection(B))
---
+++ src/01Jan2026/sets/sets_function/mathematical_set_function/set_math.py
@@ -0,0 +1,8 @@
+A={10,20,30}
+B={50,60,70,10}
+C=A|B
+print(C)
+print(A.union(B))
+D=A&B
+print(D)
+print(A.intersection(B))
