947165229655152c0555ee429735df9f2f3c99e7
Mohammad Shahnawaz <msnazia2012@gmail.com>
2026-01-06 21:51:38 +0530

    interview

---
+++ src/06Jan2026/interview.py
@@ -0,0 +1,6 @@
+str='''The story of the Python programming language begins in the late 1980s as a hobby project by Dutch programmer Guido van Rossum. His goal was to create an easy-to-read, intuitive scripting language that could serve as a successor to the ABC language and bridge the gap between C programming and shell scripts.'''
+words= str.split(' ')
+print(words)
+for word in words:
+    print(word)
+    
