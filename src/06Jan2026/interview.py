b24250823525a69f8fd831960305c937b59193bc
Mohammad Shahnawaz <msnazia2012@gmail.com>
2026-01-06 21:56:55 +0530

    interview

--- src/06Jan2026/interview.py
+++ src/06Jan2026/interview.py
@@ -1,6 +1,12 @@
 str='''The story of the Python programming language begins in the late 1980s as a hobby project by Dutch programmer Guido van Rossum. His goal was to create an easy-to-read, intuitive scripting language that could serve as a successor to the ABC language and bridge the gap between C programming and shell scripts.'''
 words= str.split(' ')
 print(words)
+word_dict={}
 for word in words:
     print(word)
+    if word is word_dict.keys:
+        word_dict[word]=word_dict.get(word,0)+1
+    else:
+        word_dict[word]=1
     
+print(word_dict)
