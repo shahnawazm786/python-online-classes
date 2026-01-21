5597c82eef5a1d8bd08da83ff4bc142f1ea89ba7
Mohammad Shahnawaz <msnazia2012@gmail.com>
2026-01-21 22:27:39 +0530

    conversation method

---
+++ 21Jan2026/examples/strings/case_conversion.py
@@ -0,0 +1,28 @@
+name="KAZ - CLASSES" # upper case
+#upper
+if name =='kaz - classes'.upper():
+    print('KAZ')
+else:
+    print('Ohter institution')
+
+print('alphabet'.upper())
+
+print('change into lower case ')
+
+s='VARIABLE_NAME'
+
+print(f'{s} \nconvert into lower case\n {s.lower()}')
+
+# First character of each word convert into upper case 
+city='''mumbai 
+        kolkatta 
+        chennai 
+        new delhi
+    '''
+print(city.title())
+
+sentence='''kaz classes
+            teaching python
+            python is scripting language.
+        '''
+print(sentence.capitalize())
