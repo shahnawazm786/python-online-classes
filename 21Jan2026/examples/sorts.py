e5dba43ff8d2d088fdf54ba8da49e903827ddb5b
Mohammad Shahnawaz <msnazia2012@gmail.com>
2026-01-21 21:46:45 +0530

    sort

---
+++ 21Jan2026/examples/sorts.py
@@ -0,0 +1,17 @@
+L=[10,7,-1,40,3,100,13,33]
+T=(22,-5,89,900,300)
+S={12,56,8,90,34,7,22,67}
+
+
+# Question - find out the maximum value from the above List, Tuple and Set
+print('Ascending order / Increasing order [A-Z][0-9]')
+print(f'Sorted list is {sorted(L)}')
+
+print(f'Sorted Tuple is {sorted(T)}')
+
+print(f'Set is  {sorted(S)}')
+
+print('Descending / Decreasing order [Z-A][9-0]')
+
+print(f'Descending Sorted list is {sorted(L,reverse=True)}')
+
